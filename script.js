3f2d7bd9bf2238ad7aec401ba91575a2b5f342f7
Larisa Roslova <Larisa1220@mail.ru>
2024-12-18 14:01:19 +0300

    fix comments

--- script.js
+++ script.js
@@ -2,16 +2,6 @@
 // Сделать приложение "CALCULATOR" и попробовать максимально близко повторить функционал. За образец работы можно взять дефолтный калькулятор windows, с уменьшенным кол-вом кнопок (образец для кнопок берем из прикрепленного изображения). Метод eval - использовать нельзя.
 // Имя ветки выбираем любое, например feature. В ветке создаем 3 файла: js, html, css. (пишем на JS, сборщики, препроцессоры и т.д не используем). В визуализацию  не углубляемся. Далее делаем PR в main(master) ветку и ссылку скидываем мне. Ссылку нужно скинуть до 9.00 20 декабря.
 
-
-// document.addEventListener('DOMContentLoaded', function(e){
-//     e.preventDefault();
-    
-// })
-
-console.log('in script');
-console.log('loaded');
-
-
 let screen = document.querySelector('.calculator-screen');
 let btn_num = document.querySelectorAll('.number');
 let reset = document.querySelector('.clear');
@@ -31,11 +21,9 @@ let next_op_flag = false;
 
 
 for(let i = 0; i < btn_num.length; i++){
-    // console.log(i);
     btn_num[i].addEventListener('click', function(){
         displayValue += btn_num[i].value;
         screen.value = check_start_zero(displayValue);
-
     })
 }
 
@@ -60,28 +48,26 @@ dot.addEventListener('click', function(){
 })
 
 for(let i = 0; i < operators.length; i++){
-    // console.log(i);
     operators[i].addEventListener('click', function(){
         displayValue += operators[i].value;
         screen.value = check_operation(displayValue, operators[i].value);
     })
 }
 
-
 equal.addEventListener('click', function(){
     readB(displayValue);
     switch (operation.textContent) {
         case '+':
           result = (+a.textContent) + (+b.textContent);
-          screen.value = `= ${result}`;
+          screen.value = `=${cut_result(result)}`;
           break;
         case '-':
             result = (+a.textContent) - (+b.textContent);
-            screen.value = `= ${result}`;
+            screen.value = `=${cut_result(result)}`;
           break;
         case '*':
             result = (+a.textContent) * (+b.textContent);
-            screen.value = `= ${result}`;
+            screen.value = `=${cut_result(result)}`;
           break;
         case '/':
             if(b.textContent == '0') {
@@ -94,17 +80,23 @@ equal.addEventListener('click', function(){
                 return;
             }
             result = (+a.textContent) / (+b.textContent);
-            screen.value = `= ${result}`;
+            screen.value = `=${cut_result(result)}`;
           break;
         case '%':
             result = (+a.textContent) * (+b.textContent) / 100;
-            screen.value = `= ${result}`;
+            screen.value = `=${cut_result(result)}`;
           break;
 
     }
     next_op_flag = true;
 })
-
+function cut_result(num){
+    let char = num.toString().split('');
+    char = char.slice(0, 13);
+    // console.log(char);
+    result = +(char.join(''));
+    return result
+}
 
 function check_start_zero(str){
     let charArr = str.split('', 3);
@@ -112,12 +104,27 @@ function check_start_zero(str){
     if(charArr[0] == '0' && charArr[1] == '0'){
         displayValue = '0';
         return displayValue;
+    
     }else if(charArr[0] == '-' && charArr[1] == '0'&& charArr[2] == '0'){
         displayValue = '-0';
         return displayValue;
+    
     }else if(charArr[0] == '.'&& charArr[1]){
         displayValue = `0.${charArr[1]}`;
         return displayValue;
+    
+    }else if(charArr[0] == '0' && charArr[1]){
+        if(charArr[1] !== '0' && charArr[1] !== '.'){
+            displayValue = `${charArr[1]}`;
+            return displayValue;
+        }
+        displayValue = `${str}`;
+        return displayValue;
+    
+    }else if(charArr[0] == '-' && charArr[1] == '0' && charArr[2] !== '0' && charArr[2] !== '.' && charArr[2] !== undefined){
+        displayValue = `-${charArr[2]}`;
+        return displayValue;
+    
     }else{
         return str;
     }
@@ -129,6 +136,7 @@ function check_operation(str, op){
         displayValue = str;
         minus_flag = false;
         return displayValue;
+    
     }if(next_op_flag){
         a.textContent = result;
         b.textContent = 0;
@@ -137,6 +145,7 @@ function check_operation(str, op){
         minus_flag = true;
         displayValue = '';
         return displayValue;
+    
     }else{
         readA(charArr);
         operation.textContent = `${op}`;
@@ -153,12 +162,10 @@ function readA(arr){
     if(Number.isNaN(+a.textContent)){
         a.textContent = 0;
     }
-    console.log(+a.textContent);
 }
 
 function readB(str){
     b.textContent = displayValue;
-    console.log(+b.textContent);
 }
 
 erase.addEventListener('click', function(){
@@ -167,180 +174,3 @@ erase.addEventListener('click', function(){
     displayValue = charArr.join('');
     screen.value = displayValue;
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const calculator = {
-//     displayValue: '0',
-//     firstOperand: null,
-//     waitingForSecondOperand: false,
-//     operator: null,
-// };
-
-// function inputDigit(digit) {
-//     const { displayValue, waitingForSecondOperand } = calculator;
-
-//     if (waitingForSecondOperand === true) {
-//         calculator.displayValue = digit;
-//         calculator.waitingForSecondOperand = false;
-//     } else {
-//         calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-//     }
-// }
-
-// function inputDecimal(dot) {
-//     if (calculator.waitingForSecondOperand === true) return;
-
-//     if (!calculator.displayValue.includes(dot)) {
-//         calculator.displayValue += dot;
-//     }
-// }
-
-// function handleOperator(nextOperator) {
-//     const { firstOperand, displayValue, operator } = calculator;
-//     const inputValue = parseFloat(displayValue);
-
-//     if (operator && calculator.waitingForSecondOperand) {
-//         calculator.operator = nextOperator;
-//         return;
-//     }
-
-//     if (firstOperand == null && !isNaN(inputValue)) {
-//         calculator.firstOperand = inputValue;
-//     } else if (operator) {
-//         const result = performCalculation[operator](firstOperand, inputValue);
-
-//         calculator.displayValue = String(result);
-//         calculator.firstOperand = result;
-//     }
-
-//     calculator.waitingForSecondOperand = true;
-//     calculator.operator = nextOperator;
-// }
-
-// const performCalculation = {
-//     '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
-//     '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
-//     '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
-//     '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
-//     '=': (firstOperand, secondOperand) => secondOperand
-// };
-
-// function resetCalculator() {
-//     calculator.displayValue = '0';
-//     calculator.firstOperand = null;
-//     calculator.waitingForSecondOperand = false;
-//     calculator.operator = null;
-// }
-
-// function updateDisplay() {
-//     const display = document.querySelector('.calculator-screen');
-//     display.value = calculator.displayValue;
-// }
-
-// updateDisplay();
-
-// const keys = document.querySelector('.calculator-keys');
-// keys.addEventListener('click', (event) => {
-//     const { target } = event;
-//     if (!target.matches('button')) {
-//         return;
-//     }
-
-//     if (target.classList.contains('operator')) {
-//         handleOperator(target.value);
-//         updateDisplay();
-//         return;
-//     }
-
-//     if (target.classList.contains('decimal')) {
-//         inputDecimal(target.value);
-//         updateDisplay();
-//         return;
-//     }
-
-//     if (target.classList.contains('all-clear')) {
-//         resetCalculator();
-//         updateDisplay();
-//         return;
-//     }
-
-//     inputDigit(target.value);
-//     updateDisplay();
-// });
